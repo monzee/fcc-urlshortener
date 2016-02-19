c7e08f794b7e58806f8abe05f280e69785f7a3d2
Mon Zafra <monzee@gmail.com>
2016-02-19 09:28:31 +0800

    implemented date route

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var app = express();
+'use strict';
+const express = require('express');
+const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', (_, res) => {
+  res.render('pages/help');
 });
 
-app.listen(app.get('port'), function() {
+const MONTHS = `January February March April May June July August 
+                September October November December`.split(/\s+/);
+
+app.get('/:date', (req, res) => {
+    let d = req.params['date'];
+    let date = new Date(isNaN(d) ? d : +d);
+    let unix = null, natural = null;
+    if (date != 'Invalid Date') {
+        unix = +date;
+        natural = `${MONTHS[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+    }
+    res.json({unix, natural});
+});
+
+app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
