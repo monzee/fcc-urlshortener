06ad9f1f2503df55bf068c7e1800a65cadb6d7ff
Mon Zafra <monzee@gmail.com>
2016-02-22 10:19:43 +0800

    implemented url shortener

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 'use strict';
+const urlValidator = require('valid-url');
 const express = require('express');
 const app = express();
+
+const DATABASE = [];  // don't hate. this is a toy.
+
+function baseUrl(req) {
+    return `${req.protocol}://${req.get('host')}`;
+}
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,15 +17,29 @@ app.use(express.static(`${__dirname}/public`));
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
 
-app.get('/', (_, res) => {
-    res.render('pages/usage');
+app.get('/', (req, res) => {
+    let path = req.url.replace(/\/*$/, '');
+    res.render('pages/usage', {baseUrl: baseUrl(req) + path});
 });
 
-app.get('/api/whoami', (req, res) => {
-    let ipaddress = req.connection.remoteAddress.match(/:([^:]+)$/)[1] || req.connection.remoteAddress;
-    let language = req.headers['accept-language'].split(',')[0];
-    let software = req.headers['user-agent'].match(/\(([^)]+)\)/)[1] || req.headers['user-agent'];
-    res.json({ipaddress, language, software});
+app.get('/new/*', (req, res) => {
+    let fullUrl = req.params[0];
+    if (req.query.allow || urlValidator.isWebUri(fullUrl)) {
+        let id = DATABASE.length;
+        DATABASE.push(fullUrl);
+        res.json({original_url: fullUrl, short_url: `${baseUrl(req)}/${id}`});
+    } else {
+        res.status(500).json({error: "Invalid URL"});
+    }
+});
+
+app.get('/:id(\\d+)', (req, res) => {
+    let id = req.params.id;
+    if (id in DATABASE) {
+        res.redirect(DATABASE[id]);
+    } else {
+        res.status(404).json({error: "Unregistered short URL"});
+    }
 });
 
 app.listen(app.get('port'), () => {
